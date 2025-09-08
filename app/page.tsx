2cc12d0588a6cd9628591b6a3c8d80eaa3a675c7
melisa uyar <melisauyar5225@gmail.com>
2025-09-08 19:16:33 +0300

    Companion cards created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,58 @@
-import { Button } from "@/components/ui/button"
 import React from 'react'
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
   return (
-    <div>
-        <h1 className="text-2xl underline">Hello</h1>
-        <button className="bg-blue-500 text-white px-4 py-2 rounded">Click me</button>
-        <Button>start</Button>
+    <main>
+        <h1 >Popular Companions</h1>
 
-    </div>
+        <section className="home-section">
+            <CompanionCard
+
+                id="123"
+                name="Neura the Brainy Explorer"
+                topic="Neural Network of the Brain"
+                subject="science"
+                duration="45"
+                color="#ffda6e"
+
+            />
+            <CompanionCard
+
+                id="456"
+                name="Countsy the Number Wizard"
+                topic="Topic: Derivatives & Integrals"
+                subject="Maths"
+                duration="30"
+                color="#FFDA6E"
+            />
+            <CompanionCard
+
+                id="789"
+                name="Verba the Vocabulary Builder"
+                topic="Topic: English Literature "
+                subject="Language"
+                duration="30"
+                color="#BDE7FF"
+            />
+
+
+        </section>
+        <section className="home-section">
+            <CompanionsList
+                title = "Recently completed sessions"
+                companions={recentSessions}
+                classNames="w-2/3 max-lg:w-full"
+
+            />
+            <CTA />
+
+        </section>
+
+    </main>
   )
 }
 
---
+++ components/CompanionsList.tsx
@@ -0,0 +1,62 @@
+import {
+    Table,
+    TableBody,
+    TableCaption,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import Link from "next/link";
+import {cn, getSubjectColor} from "@/lib/utils";
+import Image from "next/image";
+
+
+interface CompanionsListProps {
+    title: string;
+    companions?: Companion[];
+    classNames?: string;
+}
+
+const CompanionsList = ({ title, companions, classNames}: CompanionsListProps) => {
+
+    return (
+        <article className={cn('companion-list', classNames)}>
+            <h2 className="font-bold text-3xl">Recent Sessions</h2>
+            <Table>
+
+                <TableHeader>
+                    <TableRow>
+                        <TableHead className="text-lg w-2/3">Lessons</TableHead>
+                        <TableHead className="text-lg">Subject</TableHead>
+                        <TableHead className="text-lg text-right" >Duration</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {companions?.map(({ id, subject, name, topic, duration }) => (
+
+                        <TableRow key={id}>
+                            <TableCell>
+                                <Link href={`/companions/${id}`}>
+                                    <div className="flex items-center gap-2">
+                                        <div className="size-[72px] flex items-center justify-center
+
+                                        rounded-lg max-md:hidden" style={{backgroundColor:getSubjectColor(subject)}}>
+                                            <Image src={`/icons/${subject}.svg`}
+                                                   alt={subject} width={35}
+                                                   height={35}/>
+                                        </div>
+
+                                    </div>
+                                </Link>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+
+                </TableBody>
+            </Table>
+
+        </article>
+    )
+}
+export default CompanionsList
